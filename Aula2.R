74db7f6d16134f68da688d46a3cc6c4094983ee0
JuliaCansado <julia.cansado@usp.br>
2022-02-09 00:18:00 -0300

    Track Aula 2

---
+++ Aula2.R
@@ -0,0 +1,11 @@
+usethis::use_git_config(
+  user.name = "JuliaCansado",
+  user.email = "julia.cansado@usp.br"
+)
+usethis::create_github_token()
+
+gitcreds::gitcreds_set()
+#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
+usethis::git_sitrep()
+usethis::use_git()
+usethis::use_github()
