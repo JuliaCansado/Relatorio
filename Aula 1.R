cda4930e8a6694c8ef5cafd576d43225e10fe2c5
JuliaCansado <julia.cansado@usp.br>
2022-02-09 00:11:51 -0300

    Initial commit

---
+++ Aula 1.R
@@ -0,0 +1,4 @@
+caminho <- "c:/Users/julia/Desktop/Training/Relatorio/dados/pinguins.csv" 
+#lembrar que o caminho deve ir até o arquivo, não só até a pasta que contém o arquivo
+pinguins <- readr::read_csv(caminho)
+
